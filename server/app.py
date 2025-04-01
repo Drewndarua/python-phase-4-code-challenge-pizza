95a780a368128c751bb5afdd5cf0f8c26881bff5
Andrew Kinyua <andrewkinyua500@gmail.com>
2025-04-02 02:08:42 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from models import db, Restaurant, RestaurantPizza, Pizza
+from models import db, Restaurant, RestaurantPizza
 from flask_migrate import Migrate
 from flask import Flask, request, make_response
 from flask_restful import Api, Resource
@@ -20,9 +20,42 @@ db.init_app(app)
 api = Api(app)
 
 
-@app.route("/")
-def index():
-    return "<h1>Code challenge</h1>"
+@app.route("/restaurants/<int:id>", methods=["GET"])
+def get_restaurants(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        return make_response(restaurant.to_dict(rules=("restaurant_pizzas",)), 200)
+    return make_response({"error": "Restaurant not found"}, 404)
+
+
+@app.route("/restaurant/<int:id>", methods=["DELETE"])
+def delete_restaurants(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response({}, 204)
+    return make_response({"error": "Restaurant not found"}, 404)
+
+
+@app.route("/restaurant_pizzas", methods=["POST"])
+def create_restaurant_pizza():
+    try:
+        data = request.get_json()
+        new_restaurant_pizza = RestaurantPizza(
+            price=data["price"],
+            restaurant_id=data["restaurant_id"],
+            pizza_id=data["pizza_id"],
+        )
+        db.session.add(new_restaurant_pizza)
+        db.session.commit()
+        return make_response(new_restaurant_pizza.to_dict(), 201)
+    
+    except ValueError as e:
+        return make_response({"errors": [str(e)]}, 400) 
+
+    except Exception:
+        return make_response({"errors": ["Invalid data"]}, 400)
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -17,12 +17,12 @@ class Restaurant(db.Model, SerializerMixin):
     __tablename__ = "restaurants"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    address = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
 
-    # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', cascade='all, delete')
 
-    # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -35,9 +35,9 @@ class Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-    # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
 
-    # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza' ,)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -49,11 +49,16 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
-    # add relationships
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
 
-    # add serialization rules
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
 
-    # add validation
+    @validates('price')
+    def validate_prices(self, key, price):
+        if not(1 <= price <= 30):
+            raise ValueError('Invalid price amount')
+        return price
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
